cd461dc24e32e7ff77bcae2d1f0b32045ac2e759
2x5acles <annisacarter123@gmail.com>
2025-04-29 12:04:47 +0000

    completed fucntion 2

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ const greet = (name) => {
     return "Hello," + name + "!"
 }
 const greet = name => "Hello" + name + "!";
+
+//Function 2
+
+function square (number){
+    return number * number;
+}
+
+const square = number => {
+    return number * number;
+}
+const square = number => number * number;
+
+//function 3
