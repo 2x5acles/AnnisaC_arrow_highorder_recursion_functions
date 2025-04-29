3369839d8862be655a05c773a4b6c67309c81077
2x5acles <annisacarter123@gmail.com>
2025-04-29 11:58:46 +0000

    completed function 1

---
+++ script.js
@@ -0,0 +1,9 @@
+//Function 1 
+function greet(name){
+    return "Hello," + name + "!";
+
+}
+const greet = (name) => {
+    return "Hello," + name + "!"
+}
+const greet = name => "Hello" + name + "!";
