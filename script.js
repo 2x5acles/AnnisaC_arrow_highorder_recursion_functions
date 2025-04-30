3d0c1041ca9f985e49342118c8d0e535845a4bad
2x5acles <annisacarter123@gmail.com>
2025-04-30 21:38:56 +0000

    x

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+
+//excercise #1 arrow functions
+
 //Function 1 
 function greet(name){
     return "Hello," + name + "!";
@@ -39,5 +43,48 @@ const isEven = (number) => {
     };
 
     const isEVen = number => number %2 === 0;
+
+    //Exercise #2 High Order Function 
     
 
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const evenNumbersSquared = numbers => {
+  return numbers
+    .filter(num => num % 2 === 0)
+    .map(num => num ** 2);
+};
+
+console.log(evenNumbersSquared(numbers)); // Output: [4, 16, 36, 64, 100]
+
+//PART 2 
+
+const prices  = [10.99, 5.49, 12.99, 8.75, 15.20];
+const calculateDiscountedTotal = prices => {
+    return prices.reduce((total, price) => {
+      const discounted = price * 0.8;
+      console.log(`Original: $${price} → Discounted: $${discounted}`);
+      return total + discounted;
+    }, 0);
+  };
+
+const words =["apple", "banana", "cherry", "date",  "elderberry"];
+
+const findWordsWithA = words => {
+    return words.find(word => word.includes('a'));
+
+};
+
+console.log(findWordsWithA(words));  // output apple
+
+
+//Excercise 3 
+
+
+Const sumofArray = (arrayOfNumbers) => {
+    if (arrayOfNumbers.length === 0) {
+        return 0;
+    }
+    return arrayOfNumbers[0] + sumofArray(arrayOfNumbers.slice(1))
+}
+]
